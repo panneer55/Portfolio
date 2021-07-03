50e89993a350608ef3e4547eed18b2383de72034
panneer55 <81272557+panneer55@users.noreply.github.com>
2021-07-03 20:16:17 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $("header").addClass('sticky')
+        }
+        else{
+            $("header").removeClass("sticky")
+        }
+    })
+})
